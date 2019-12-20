ab5127bca834cf782e963b4de01b87ece8c10dcc
tofanijo <tofanijo@gmail.com>
2019-12-20 19:09:53 +0200

    Ciklas

---
+++ NR 6/NR 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius1 = 0;
+            int suma = 0;
+
+            Console.WriteLine("Iveskite bet koki skaiciu");
+            if (!int.TryParse(Console.ReadLine(), out skaicius1))
+            {
+                Console.WriteLine("Ivesti neteisingi duomenis");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            for (int i = 1; i <= skaicius1; i++)
+            {
+                Console.WriteLine(i);
+                suma = i + suma;
+                Console.WriteLine(suma);
+            }
+            Console.ReadLine();
+        }
+    }
+}
