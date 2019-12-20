7f6538c9fb2ea7804eaa938fec5be746e425d856
tofanijo <tofanijo@gmail.com>
2019-12-20 18:26:34 +0200

    NR 2

---
+++ NR 2/NR 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+
+                Console.WriteLine(c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
