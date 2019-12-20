9824696264c90f19a5dad8829bd49e699ca68b02
tofanijo <tofanijo@gmail.com>
2019-12-20 18:47:31 +0200

    NR 5

---
+++ NR 5/NR 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NR_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //progres bar
+            for (int i = 0; i < 50; i++)
+            {
+                //reikia prideti usig System.Threading;
+                Thread.Sleep(100); //milisekundes
+                System.Console.Write("#"); 
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pabaiga");
+        }
+    }
+}
