47bc93157c0d6b4ccbf85320f40fc9baa8399108
tofanijo <tofanijo@gmail.com>
2019-12-20 19:26:59 +0200

    NR 2.2

---
+++ NR 2.2/NR 2.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius = 0;
+            int suma = 0;
+
+            Console.WriteLine("Iveskite bet koki skaiciu");
+            if (!int.TryParse(Console.ReadLine(), out skaicius))
+            {
+                Console.WriteLine("Ivesti neteisingi duomenis");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            for (int i = 1; i < skaicius; i+=2)
+            {
+                Console.WriteLine(i);
+                
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
