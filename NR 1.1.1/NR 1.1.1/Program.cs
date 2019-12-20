c010b519101cf7ca0f113593b3e225b038392b45
tofanijo <tofanijo@gmail.com>
2019-12-20 21:17:21 +0200

    NR 2.2.2

---
+++ NR 1.1.1/NR 1.1.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] skaiciai = new double[5];
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("Iveskite skaicius {0}", i + 1);
+                skaiciai[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+
+
+            for (int i = skaiciai.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(skaiciai[i]);
+            }
+            Console.ReadLine();
+        }
+
+       
+    }
+}
