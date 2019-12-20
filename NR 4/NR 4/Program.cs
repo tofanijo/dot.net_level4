d4675931baced4934490ff7f24870eb9dbe0884b
tofanijo <tofanijo@gmail.com>
2019-12-20 18:39:46 +0200

    lengt

---
+++ NR 4/NR 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "CKursas";
+            for (int i = 0; i < vardas.Length; i++)
+            {
+                char current = vardas[i];
+                Console.WriteLine(current);
+            }
+            Console.ReadLine();
+                }
+    }
+}
