ab0aa16ebc0dc09db0df29a7c96b77ce9c355711
tofanijo <tofanijo@gmail.com>
2019-12-20 20:05:52 +0200

    masyvas su streng

---
+++ 7/7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] savaitesDienos =
+            {
+                "Pirmadienis",
+                 "Antradienis",
+                  "Treciadienis",
+                   "Ketvirdatienis",
+                    "Penktadienis",
+                     "Sestadienis",
+                      "Sekmadienis",
+            };
+
+            for (int i = 0; i < savaitesDienos.Length; i++)
+            {
+                Console.WriteLine(savaitesDienos[i]);
+
+            }
+            Console.ReadLine();
+
+            }
+        }
+    }
+
+
+
