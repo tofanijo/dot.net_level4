9f32be0f79fa65e1de1defcccccd40bef4b3d219
tofanijo <tofanijo@gmail.com>
2019-12-20 20:36:08 +0200

    NR 9

---
+++ 9/9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //masyvo uzpildymas atsistiktiniais skaiciais
+
+            int[] skaiciai = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < skaiciai.Length; i++)
+            {
+
+                skaiciai[i] = rnd.Next(1, 100);
+            }
+            foreach (var skaicius in skaiciai)
+            {
+                Console.WriteLine("{0}, ", skaicius);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
