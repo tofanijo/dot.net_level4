30f3126a8326ac5bd0613ff89cc81db6ed01d4b5
tofanijo <tofanijo@gmail.com>
2019-12-20 20:26:22 +0200

    masyvas

---
+++ 8/8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] savaitesDienos =
+            {
+                "Pirmadienis",
+                 "Antradienis",
+                  "Treciadienis",
+                   "Ketvirdatienis",
+                    "Penktadienis",
+                     "Sestadienis",
+                      "Sekmadienis",
+            };
+
+            Console.WriteLine("Pries:");
+            foreach (var diena in savaitesDienos)
+            {
+                Console.WriteLine(diena);
+            }
+            savaitesDienos[2] = "TRECIADIENIS";
+
+                Console.WriteLine("");
+            Console.WriteLine("Po:");
+            foreach (var diena in savaitesDienos)
+            {
+                Console.WriteLine(diena);
+            }
+            Console.ReadLine();
+        }
+    }
+}
