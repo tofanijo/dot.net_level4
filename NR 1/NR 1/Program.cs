a1c619cf591378d2d17288749da7924692a58441
tofanijo <tofanijo@gmail.com>
2019-12-20 18:17:15 +0200

    mazejantis cikas

---
+++ NR 1/NR 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10 - 1; i >= 0; i --)
+            {
+                Console.WriteLine("reiksme i : {0}", i);
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
