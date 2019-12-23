669f023d326760d3c511eee7291e2bf741ee68e7
tofanijo <tofanijo@gmail.com>
2019-12-23 10:05:01 +0200

    Ivesti skaiciai masyve

---
+++ NR 3.3.3/NR 3.3.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_3._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] ivestiSkaiciai = new double[5];
+
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("Iveskite skaicius {0}", i + 1);
+                ivestiSkaiciai[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("Pries:");
+            foreach (var skaicius in ivestiSkaiciai)
+            {
+                Console.WriteLine(skaicius);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
