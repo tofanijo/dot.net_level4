10a829001552a4025eb690fa43d7705390c610ba
tofanijo <tofanijo@gmail.com>
2019-12-23 09:33:08 +0200

    suma per foreach

---
+++ NR 2.2.2/NR 2.2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_2._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] skaiciai = new double[6];
+            double suma = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+
+                Console.WriteLine("Iveskite skaicius {0}", i + 1);
+                skaiciai[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+                foreach(double skaicius in skaiciai)
+            {
+                suma += skaicius;
+            }
+                {
+                Console.WriteLine("Skaiciu suma: {0}", suma);
+
+                }
+            Console.ReadLine();
+            }
+        }
+    }
+
